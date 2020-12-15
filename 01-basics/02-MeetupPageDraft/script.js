5fdd40911236c6acb6a2594dcdf0d9c1cb498943
orlovse <hajoff@gmail.com>
2020-12-15 22:13:55 +0200

    add solution 02-CounterButton and 02-MeetupPage

--- 01-basics/02-MeetupPageDraft/script.js
+++ 01-basics/02-MeetupPageDraft/script.js
@@ -15,6 +15,13 @@ function getMeetupCoverLink(meetup) {
   return `${API_URL}/images/${meetup.imageId}`;
 }
 
+const getDateOnlyString = (date) => {
+  const YYYY = date.getUTCFullYear();
+  const MM = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const DD = date.getUTCDate().toString().padStart(2, '0');
+  return `${YYYY}-${MM}-${DD}`;
+}
+
 /**
  * Словарь заголовков по умолчанию для всех типов элементов программы
  */
@@ -47,20 +54,54 @@ const agendaItemIcons = {
 export const app = new Vue({
   el: '#app',
 
-  data: {
-    //
+  data() {
+    return {
+      originalMeetup: null,
+      loading: false,
+      img: null,
+    };
   },
 
-  mounted() {
+  async mounted() {
     // Требуется получить данные митапа с API
+    this.loading = true;
+    this.originalMeetup = await this.fetchData();
+    this.loading = false;
+    this.img = getMeetupCoverLink(this.meetup);
   },
 
   computed: {
-    //
+    meetup() {
+      const meetup = { ...this.originalMeetup };
+      meetup.cover = this.img ? { '--bg-url': `url(${this.img})` } : {};
+
+      meetup.localDate = new Date(meetup.date).toLocaleString(
+        navigator.language, {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric',
+      });
+
+      meetup.dateOnlyString = getDateOnlyString(new Date(meetup.date));
+
+      if (!meetup.title) {
+        meetup.title = agendaItemTitles[meetup.type];
+      }
+
+
+
+      return meetup;
+    }
   },
 
   methods: {
-    // Получение данных с API предпочтительнее оформить отдельным методом,
-    // а не писать прямо в mounted()
+    fetchData() {
+      return fetch(`${API_URL}/meetups/${MEETUP_ID}`).then((res) => res.json());
+    },
+
+    getIcon(item) {
+      const icon = agendaItemIcons[item.type] || 'cal-sm';
+      return `/assets/icons/icon-${icon}.svg`;
+    }
   },
 });
