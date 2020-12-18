9b0f37a05af7710af968b0f3056990c612e2dc1c
orlovse <hajoff@gmail.com>
2020-12-18 16:42:31 +0200

    add solution to 02-components/01-CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -1,10 +1,28 @@
 export const CounterButton = {
   // Шаблон потребуется отредактировать
-  template: '<button type="button"></button>',
+  template: '<button type="button" @click="handleClick">{{ count }}</button>',
 
-  // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
 
-  // Компонент должен иметь модель
+  model: {
+    prop: "count",
+    event: "increment"
+  },
 
-  // Шаблон лучше держать максимально простым, а логику выносить в методы
+  mounted() {
+    if (!this.count){
+      this.$emit("increment", 0)
+    }
+  },
+
+  methods: {
+    handleClick() {
+      this.$emit("increment", this.count + 1)
+    },
+  },
 };
