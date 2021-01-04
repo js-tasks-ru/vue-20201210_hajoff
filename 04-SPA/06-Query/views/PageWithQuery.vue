3d2b89339400b4bdb55c680c99d3b39fbf8f42be
orlovse <hajoff@gmail.com>
2021-01-04 10:05:59 +0200

    add solution to 04-SPA/06-Query

--- 04-SPA/06-Query/views/PageWithQuery.vue
+++ 04-SPA/06-Query/views/PageWithQuery.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <meetups-view />
+    <meetups-view
+      :view.sync="view"
+      :date.sync="date"
+      :participation.sync="participation"
+      :search.sync="search"
+    />
   </div>
 </template>
 
@@ -9,7 +14,71 @@ import MeetupsView from '../components/MeetupsView';
 
 export default {
   name: 'PageWithQuery',
-  components: { MeetupsView },
+  components: {
+    MeetupsView,
+  },
+
+  data() {
+    return {
+      defaultQuery: {
+        view: 'list',
+        date: 'all',
+        participation: 'all',
+        search: '',
+      },
+    };
+  },
+
+  computed: {
+    view: {
+      get() {
+        return this.getParam('view');
+      },
+      set(value) {
+        this.setParam('view', value);
+      },
+    },
+    date: {
+      get() {
+        return this.getParam('date');
+      },
+      set(value) {
+        this.setParam('date', value);
+      },
+    },
+    participation: {
+      get() {
+        return this.getParam('participation');
+      },
+      set(value) {
+        this.setParam('participation', value);
+      },
+    },
+    search: {
+      get() {
+        return this.getParam('search');
+      },
+      set(value) {
+        this.setParam('search', value);
+      },
+    },
+  },
+  methods: {
+    getParam(name) {
+      return this.$route.query[name] || this.defaultQuery[name];
+    },
+    setParam(name, value) {
+      if (value === this[name]) {
+        return;
+      }
+      let query = { ...this.$route.query };
+      delete query[name];
+      if (value != this.defaultQuery[name]) {
+        query[name] = value;
+      }
+      this.$router.push({ query });
+    },
+  },
 };
 </script>
 
